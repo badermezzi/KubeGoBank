9d2afe0da35ea162b99905579240e529f2604581
badermezzi <mezzibader159@gmail.com>
2025-03-04 22:09:16 +0000

    GetAccountApi testing

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("cannot load config:", err) // Exit if connection fails
+		log.Fatal("cannot load config:", err) 
 	}
 
 	connection, err := sql.Open(config.DBDriver, config.DBSource) // Open database connection
